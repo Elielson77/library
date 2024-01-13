877a526581986a9c6b7e405f3b8ad50462b6e4f2
Elielson77 <elielsonll77@gmail.com>
2024-01-13 09:31:54 -0300

    feature: initial config to allow image upload

--- books/models.py
+++ books/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from uuid import uuid4
 
 
+def upload_image_book(instance, file_name):
+    return f"{instance.id_book}-{file_name}"
+
+
 # Create your models here.
 class Books(models.Model):
     id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -12,3 +16,4 @@ class Books(models.Model):
     pages = models.IntegerField()
     publishing_company = models.CharField(max_length=255)
     created_at = models.DateField(auto_now_add=True)
+    img = models.ImageField(upload_to=upload_image_book, blank=True, null=True)
